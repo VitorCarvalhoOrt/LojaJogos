1151ee3122015da4a87b41fc67c81e1967e552f8
Vitor Barsali <vitor.carvalho@ort.org.br>
2026-06-22 17:50:55 -0300

    refeito o que esqueci de dar commit: models e parte do admin

--- catalogo/admin.py
+++ catalogo/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from catalogo.models import *
+
 # Register your models here.
+
+admin.site.register(Desenvolvedor)
+admin.site.register(Plataforma)
+admin.site.register(Jogo)
--- catalogo/models.py
+++ catalogo/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+class Desenvolvedor(models.Model):
+    nome = models.CharField(max_length=128)
+    motor_jogos = models.CharField(max_length=128)
+    ano_fundada = models.DateField()
+
+    def __str__(self):
+        return self.nome
+
+class Plataforma(models.Model):
+    tipo = models.CharField(max_length=32)
+    nome = models.CharField(max_length=128)
+    ano_lancamento = models.DateField()
+    geracao = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return self.nome
+
+class Jogo(models.Model):
+    titulo = models.CharField(max_length=128)
+    descricao = models.TextField()
+    genero = models.CharField(max_length=128)
+    lancamento = models.DateField()
+
+    desenvolvedor = models.ForeignKey(Desenvolvedor, on_delete=models.SET_NULL, null=True)
+
+    plataforma = models.ManyToManyField(Plataforma)
+
+    def __str__(self):
+        return self.titulo
